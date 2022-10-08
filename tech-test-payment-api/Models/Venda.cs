2a6459d8088e2598e690b4023f93145f1c594b1d
fq38 <fq38.live.com>
2022-10-07 23:51:09 -0300

    Adicionar arquivos de projeto.

---
+++ tech-test-payment-api/Models/Venda.cs
@@ -0,0 +1,6 @@
+﻿namespace tech_test_payment_api.Models
+{
+    public class Venda
+    {
+    }
+}
---
+++ tech-test-payment-api/Models/Vendedor.cs
@@ -0,0 +1,12 @@
+﻿namespace tech_test_payment_api.Models
+{
+    public class Vendedor
+    {
+        public int id { get; set; }
+        public string cpf { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string telefone { get; set; }
+
+    }
+}
